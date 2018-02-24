80d3cfb467fdef3173e42b3e023e3da813ce6b2e
Robin Larsson <robin.larsson@kaustik.com>
2018-02-24 12:12:37 +0100

    Moving generic function to own file.

--- js/application.js
+++ js/application.js
@@ -87,20 +87,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 }, false);
-
-function strip(str) {
-    // Remove some tags
-    str = str.replace(/<[^>]+>/gim, '');
-
-    // Remove BB code
-    str = str.replace(/\[(\w+)[^\]]*](.*?)\[\/\1]/g, '$2 ');
-
-    // Remove html and line breaks
-    const div = document.createElement('div');
-    div.innerHTML = str;
-
-    const input = document.createElement('input');
-    input.value = div.textContent || div.innerText || '';
-
-    return input.value.trim();
-}
---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function strip(str) {
+    // Remove some tags
+    str = str.replace(/<[^>]+>/gim, '');
+
+    // Remove BB code
+    str = str.replace(/\[(\w+)[^\]]*](.*?)\[\/\1]/g, '$2 ');
+
+    // Remove html and line breaks
+    const div = document.createElement('div');
+    div.innerHTML = str;
+
+    const input = document.createElement('input');
+    input.value = div.textContent || div.innerText || '';
+
+    return input.value.trim();
+}
