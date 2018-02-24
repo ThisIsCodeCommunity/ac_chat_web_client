3f85f579b712a4d9fdd1a1d63ced1fe2087e8b44
tochman <thomas@craftacademy.se>
2018-02-24 17:00:43 +0100

    adds promise to getRoom function

--- js/getRooms.js
+++ js/getRooms.js
@@ -1,14 +1,16 @@
 function getRooms() {
     "use strict";
-    fetch(getRoomsUrl, {
-        headers: new Headers({
-            'Accept': 'application/json'
-        })
-    }).then(function (response) {
-        response.json().then(function (data) {
-            debugger;
 
-            return data;
-        })
-    });
+    return new Promise(function(resolve){
+        fetch(getRoomsUrl, {
+            headers: new Headers({
+                'Accept': 'application/json'
+            })
+        }).then(function (response) {
+            response.json().then(function (data) {
+                resolve(data);
+            })
+        });
+    })
+
 }
