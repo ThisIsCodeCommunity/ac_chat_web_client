5e28da316c4a682e67cd10f5b3afb1ff4c8c63d9
Vilberg Eliasson <vilbergs@gmail.com>
2018-02-24 16:27:16 +0100

    Use yellow box in message instead

--- js/analyzer.js
+++ js/analyzer.js
@@ -6,8 +6,14 @@ Analyzer.prototype.analyze = function() {
 
     var callback = function (request) {
         var messageData = JSON.parse(request.responseText);
+
+        var parent = document.getElementById('messages');
+        var newChild = '<div class="alert-box">' + 
+            '<p>Please consider using a more pleasant linguistics<p>'
+        '</div>';
         if (messageData.results.value === 'Adult') {
-            alert('Please consider using a more pleasant linguistics');
+            parent.insertAdjacentHTML('beforeend', newChild);
+            //alert('Please consider using a more pleasant linguistics');
             return false;
         }
         return true;
