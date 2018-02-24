5bebb05d92ff9ae6dba5ed47dabe1f6d1f37f9ac
Robin Larsson <robin.larsson@kaustik.com>
2018-02-24 15:20:49 +0100

    Removing unused listener.

--- js/listeners.js
+++ js/listeners.js
@@ -2,21 +2,4 @@ document.addEventListener('DOMContentLoaded', function () {
     if (Notification.permission !== "granted") {
         Notification.requestPermission();
     }
-
-    var input = document.getElementById('message');
-    input.addEventListener('keydown', function () {
-        /*if (input.value.length >= 10) {
-            var params = {analysis: {resource: input.value, category: 'text'}};
-            var request = new XMLHttpRequest();
-            request.open("POST", "https://ca-image-analyzer.herokuapp.com/api/analyses", true);
-            request.setRequestHeader("Content-type", "application/json");
-            request.send(JSON.stringify(params));
-            request.addEventListener("load", function () {
-                var messageData = JSON.parse(request.responseText);
-                if (messageData.results.value === 'Adult') {
-                    console.log('no can do!')
-                }
-            });
-        }*/
-    });
 }, false);
