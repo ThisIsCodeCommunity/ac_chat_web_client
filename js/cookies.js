9ba768fa4d94d3f5286690e3def460f78bcc4cca
Thomas Ochman <thomas@agileventures.org>
2023-05-23 06:11:06 +0200

    refactors cookies function: Used ES6 classes Handled optional arguments with default parameters Used template literals Removed unnecessary Promises

--- js/cookies.js
+++ js/cookies.js
@@ -1,22 +1,45 @@
-function Cookies() {}
+/**
+ * Cookies Class for setting and getting cookies in a web browser
+ */
+class Cookies {
+    /**
+     * Set a cookie.
+     * @param {string} name - The name of the cookie.
+     * @param {string} value - The value of the cookie.
+     * @param {number} [days=7] - The number of days until the cookie will expire. Defaults to 7 days.
+     * @param {string} [path='/'] - The path where the cookie is valid. Defaults to all paths ('/').
+     */
+    set(name, value, days = 7, path = '/') {
+        const expires = new Date(Date.now() + days * 864e5).toUTCString();
+        document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=${path}`;
+    }
 
-Cookies.prototype.set = function (name, value) {
-    var days = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 7;
-    var path = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '/';
-
-    return new Promise(function (resolve) {
-        var expires = new Date(Date.now() + days * 864e5).toUTCString();
-        document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=' + path;
-        resolve();
-    });
-};
-
-Cookies.prototype.get = function (name) {
-    return new Promise(function (resolve) {
-        var value = document.cookie.split('; ').reduce(function (r, v) {
-            var parts = v.split('=');
-            return parts[0] === name ? decodeURIComponent(parts[1]) : r
+    /**
+     * Get a cookie value.
+     * @param {string} name - The name of the cookie to get the value of.
+     * @returns {string} - The value of the cookie, or an empty string if the cookie does not exist.
+     */
+    get(name) {
+        return document.cookie.split('; ').reduce((r, v) => {
+            const parts = v.split('=');
+            return parts[0] === name ? decodeURIComponent(parts[1]) : r;
         }, '');
-        resolve(value);
-    })
-};
+    }
+}
+
+// // Usage Examples
+// let cookies = new Cookies();
+
+// // Set a cookie named "username" with value "John Doe" that will expire in 10 days.
+// cookies.set("username", "John Doe", 10);
+
+// // Get the value of the cookie named "username"
+// let username = cookies.get("username");
+// console.log(username); // prints: "John Doe"
+
+// // Set a cookie named "session" with value "abc123" that will expire in 1 day.
+// cookies.set("session", "abc123", 1);
+
+// // Get the value of the cookie named "session"
+// let session = cookies.get("session");
+// console.log(session); // prints: "abc123"
