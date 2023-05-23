0c3049513e27d434dfc88f662f0631ec9c69c179
Thomas Ochman <thomas@agileventures.org>
2023-05-23 06:22:52 +0200

    refactors the requester function Uses ES6 class Uses Promises by default, even if no callback function is provided. Adds error handling

--- js/requester.js
+++ js/requester.js
@@ -1,27 +1,50 @@
-function Requester() {}
+/**
+ * Requester Class for sending HTTP requests
+ */
+class Requester {
+    /**
+     * Log the error message for a failed XMLHttpRequest.
+     */
+    xhrError = () => {
+        console.error(this.statusText);
+    };
 
-Requester.prototype.xhrError = function() {
-    console.error(this.statusText);
-};
+    /**
+     * Send a POST request.
+     * @param {string} url - The URL to send the request to.
+     * @param {object} params - The data to send with the request.
+     * @param {function} [callbackOnLoad] - An optional callback function to call when the request loads.
+     * @returns {Promise} - A Promise that resolves with the XMLHttpRequest object when the request loads.
+     */
+    post(url, params = {}, callbackOnLoad) {
+        const request = new XMLHttpRequest();
+        request.open("POST", url, true);
+        request.setRequestHeader("Content-type", "application/json");
+        request.onerror = this.xhrError;
+        request.send(JSON.stringify(params));
 
-Requester.prototype.post = function(url, params, callbackOnLoad) {
-    var callback = callbackOnLoad || false;
-    var data = params || {};
-    var request = new XMLHttpRequest();
-    request.open("POST", url, true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.callback = callback;
-    request.onerror = this.xhrError;
-    request.send(JSON.stringify(data));
-
-    if (false !== callback) {
-        return new Promise(function (resolve) {
-            request.addEventListener("load", function () {
-                callback(request);
-                resolve(request);
-            });
+        return new Promise((resolve, reject) => {
+            request.onload = () => {
+                if (request.status >= 200 && request.status < 400) {
+                    callbackOnLoad && callbackOnLoad(request);
+                    resolve(request);
+                } else {
+                    reject(request.statusText);
+                }
+            };
+            request.onerror = () => reject(request.statusText);
         });
-    } else {
-        return request;
     }
-};
+}
+
+// // Usage Examples
+// let requester = new Requester();
+
+// // Send a POST request with some sample data
+// requester.post("https://api.example.com/data", { key: "value" })
+//     .then(response => {
+//         console.log("Request succeeded with response:", response.responseText);
+//     })
+//     .catch(error => {
+//         console.error("An error occurred:", error);
+//     });
